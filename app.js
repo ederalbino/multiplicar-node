e396762d8e8468d96a26748db253007270bf3551
edr122 <eder122albino@gmail.com>
2019-07-21 10:29:44 -0500

    Commit inicial

---
+++ app.js
@@ -0,0 +1,31 @@
+const argv = require('./config/yargs').argv;
+const colors = require('colors');
+const { crearArchivo } = require('./multiplicar/multiplicar');
+const { listarTabla } = require('./multiplicar/multiplicar');
+
+//let argv2 = process.argv; //argv de node
+let comando = argv._[0];
+
+switch (comando) {
+    case 'listar':
+        listarTabla(argv.base, argv.limite);
+        break;
+
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => {
+                console.log(`Archivo creado ${archivo.green}`);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+
+        break;
+
+    default:
+        console.log('Comando no reconocido');
+}
+
+
+
+
